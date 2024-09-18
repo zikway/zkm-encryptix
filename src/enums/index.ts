309bf133e752f76190352d32d7686fa6a1ce1ef8
CorbinV <vcorbin.wynn@gmail.com>
2024-09-18 14:49:49 +0800

    feat: 枚举类型调整

--- src/enums/index.ts
+++ src/enums/index.ts
@@ -1,9 +1,12 @@
 export enum CmdEnum {
-    KEY_CHANGE = 0xd2,
+    TEST_MODE_CHANGE = 0xd2,
     // KEY_OFF = 0xd3,
-    GET_MODE = 0xe2,
-    GET_ELEC = 0x04,
-    GET_CONN = 0xfa,
-    TEST_MODE_CHANGE = 0x19,
-    PARSE_KEY = 0x02
+    PARSE_KEY = 0x02,
+    /**
+     * @description  connected status and signal intensity
+    */
+    CONN_AND_DB = 0xfa,
+    VERSION_AND_MODE = 0xe2,
+    BATTERY = 0x04,
+    MAC_ADDRESS = 0x19
 }
