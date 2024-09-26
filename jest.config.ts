8dd582c731aeeb0dc8cda6e3b7d52aaa816a1aed
CorbinV <vcorbin.wynn@gmail.com>
2024-09-26 10:54:12 +0800

    feat: 测试配置文件接入环境变量（适配插件）

--- jest.config.ts
+++ jest.config.ts
@@ -3,6 +3,7 @@ import type { Config } from 'jest';
 const config: Config = {
   preset: 'ts-jest',
   testEnvironment: 'node',
+  setupFiles: ["dotenv/config"],
   roots: ['./test'],
   transform: {
     '^.+\\.ts$': 'ts-jest',
